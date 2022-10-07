2d9adbb9755584aef7eb1dbca893bf987b9a4fdd
Michael Mendes <michaelmendes98@hotmail.com>
2022-10-06 22:56:27 -0400

    Initial Commit

---
+++ CS3305_Assignment_2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CS3305_Assignment_2
+//
+//  Created by Michael Mendes on 2022-10-06.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
